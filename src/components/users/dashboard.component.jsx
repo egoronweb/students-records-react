3e1f0a4042f1f372956081b773455b47dd78031f
efren goron <egoronweb@gmail.com>
2022-07-28 16:42:37 +0800

    adding another color to inc

--- src/components/users/dashboard.component.jsx
+++ src/components/users/dashboard.component.jsx
@@ -116,8 +116,8 @@ class Dashboard extends React.Component{
                         <td>{item.descriptive_title}</td>
                         <td>{item.semester}</td>
                         <td>{item.year}</td>
-                        <td style={item.grade === 'inc' || item.grade === 'INC'? {color: 'yellow'} : {color: 'black'}}>{item.grade}</td>
-                        <td style={item.re_exam === 'inc' || item.re_exam === 'INC'? {color: 'yellow'} : {color: 'black'}}>{item.re_exam}</td>
+                        <td style={item.grade === 'inc' || item.grade === 'INC'? {background_color: 'yellow'} : {color: 'black'}}>{item.grade}</td>
+                        <td style={item.re_exam === 'inc' || item.re_exam === 'INC'? {background_color: 'yellow'} : {color: 'black'}}>{item.re_exam}</td>
                         <td style={remarks === "Passed"? {color:'black'} : {color:'red'}}>{remarks}</td>
                             <td>
                             <a href={`/dashboard/edit/${item.id}`}><button className="btn btn-success btn-sm btn-edit"><span className="material-symbols-outlined">edit</span></button></a>
